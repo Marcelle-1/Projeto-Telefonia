4d954e8357efe86e03eb62db96ddaae5a110df4f
Caio Ferreira <Mouraaum@gmail.com>
2023-10-15 14:42:23 -0300

    add files

---
+++ consultaMaster.php
@@ -0,0 +1,93 @@
+
+<?php
+
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "mydb";  
+
+   
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "SELECT * FROM USUARIO";
+    
+    $result = $conn->query($sql);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>consulta</title>
+    <link rel="stylesheet" href="./css/styleConsulta.css">
+</head>
+<body>
+
+
+<!--Main-->
+    <main>
+
+    <div class="div_pesquisa">
+        <input class="caixa_pesquisa" type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui..">
+    </div>
+
+    <div class="responsive_table">
+        <?php
+            if ($result->num_rows > 0) {
+                
+                echo "<table class='tabela_dados' border='1'>";
+                echo "<tr>
+                        <th>ID</th>
+                        <th>CPF</th>
+                        <th>Nome</th>
+                        <th>Sexo</th>
+                        <th>Nome_M</th>
+                        <th>Senha</th>
+                        <th>Tel_cel</th>
+                        <th>Tel_fix</th>
+                        <th>Endereço</th>
+                        <th>Status</th>
+                        <th>DT_ULT_VAL</th>
+                        <th>Data de Nascimento</th>
+                        <th>Tipo de usuario</th>
+                        <th>ações</th>
+                    </tr>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>
+                        <td>" . $row["ID"] . "</td>
+                        <td>" . $row["CPF"] . "</td>
+                        <td>" . $row["NOME"] . "</td>
+                        <td>" . $row["SEXO"] . "</td>
+                        <td>" . $row["NOME_M"] . "</td>
+                        <td>" . $row["SENHA"] . "</td>
+                        <td>" . $row["TEL_CEL"] . "</td>
+                        <td>" . $row["TEL_FIX"] . "</td>
+                        <td>" . $row["ENDERECO"] . "</td>
+                        <td>" . $row["status_USUARIO"] . "</td>
+                        <td>" . $row["DT_ULT_VAL"] . "</td>
+                        <td>" . $row["DATA_NASC"] . "</td>
+                        <td>" . $row["TIPO_USUARIO"] . "</td>
+
+
+                        <td> <a href='edit.php?id=$row[ID]' >editar</a>
+                        
+                            <a href='delete.php?id=$row[ID]' >remover</a> </td>
+
+                    </tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "Nenhum registro encontrado na tabela 'usuarios'.";
+            }
+
+            $conn->close();
+        ?>
+    </div>
+
+    </main>
+</body>
+</html>
---
+++ saveEdit.php
@@ -0,0 +1,51 @@
+<?php
+
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "mydb";  
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    if (isset($_POST['update'])) {
+
+        $id = $_POST["id"];
+        $cpf = $_POST["cpf"];
+        $nome = $_POST["nome"];
+        $sexo = $_POST["sexo"];
+        $nome_m = $_POST["nomem"];
+        $senha = $_POST["senha"];
+        $tel_cel = $_POST["telefonecelular"];
+        $tel_fix = $_POST["telefone"];
+        $endereco = $_POST["endereco"];
+        $data_nasc = $_POST["dataNasc"];
+
+    $sqlUpdate = "UPDATE USUARIO SET 
+        CPF='$cpf',
+        NOME='$nome',
+        SEXO='$sexo',
+        NOME_M='$nome_m',
+        SENHA='$senha',
+        TEL_CEL='$tel_cel',
+        TEL_FIX='$tel_fix',
+        ENDERECO='$endereco',
+        DATA_NASC='$data_nasc'
+        
+        WHERE ID='$id'";
+
+    if ($conn->query($sqlUpdate) === TRUE) {
+       
+        header('Location: consultaMaster.php');
+    } else {
+        
+        echo "Erro na atualização: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
