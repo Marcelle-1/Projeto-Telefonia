c8b4c21fa5cdbcc13de249025e18039dca5defaf
Caio Ferreira <Mouraaum@gmail.com>
2023-11-25 22:14:39 -0300

    Register / php

--- dados.php
+++ dados.php
@@ -1,76 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-
 <?php
+include_once('conexao.php');
 
-include_once("Cadastrotelefonia.html");
-var_dump($_POST);
-
-
-if (isset($_POST['cadastrar'])) {
-    // Array de erros
-    $erros = array();
-
-// Validações
-    if (!$nome = filter_input(INPUT_POST, 'nome' , ))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-    if (!$cpf = filter_input(INPUT_POST, 'cpf' , FILTER_VALIDATE_INT))
-        $erros[] = "CPF precisa ser um inteiro";
-
-// Exibir mensagem
-    if(!empty($erros)) :
-        foreach($erros as $erro) :
-            echo "<li> $erro </li>";
-        endforeach;
-    else :
-        echo "Parabéns, seus dados estão corretos";
-    endif;
     
+    $nome = $_POST['nome'];
+    $dataNasc = $_POST['dataNasc'];
+    $sexo = $_POST['sexo'];
+    $nomem = $_POST['nomem'];
+    $cpf = $_POST['cpf'];
+    $telefoneCelular = $_POST['telefoneCelular'];
+    $telefone = $_POST['telefone'];
+    $endereco = $_POST['endereco'];
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
 
-};
-
-
-// $host="localhost";
-// $port=3308;
-// $socket="";
-// $user="root";
-// $password="";
-// $dbname="mydb";
-
-// $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-//     or die ('Could not connect to the database server' . mysqli_connect_error());
-
-        
-//     //$con->close();
     
-//     if ($con->connect_errno) {
-//         echo "<br>Erro<br>";
-//         } else {
-//         echo "<br>conectado com sucesso<br>";
-//     };
+    $sql = "INSERT INTO USUARIO (NOME, DATA_NASC, SEXO, NOME_M, CPF, TEL_CEL, TEL_FIX, ENDERECO, SENHA, TIPO_USUARIO, STATUS_USUARIO, DT_ULT_VAL)
+            VALUES ('$nome', '$dataNasc', '$sexo', '$nomem', '$cpf', '$telefoneCelular', '$telefone', '$endereco', '$senha', 'COMUM', 'ATIVO', current_timestamp)";
 
+    if ($conn->query($sql) === TRUE) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar: " . $conn->error;
+    }
 
+    
+    $conn->close();
 
-   
 ?>
-</body>
-</html>
